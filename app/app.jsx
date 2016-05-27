5b5844198ffd81b445d6c880af1a0df2313884b3
Nico Haenggi <nico@haenggi.ch>
2016-05-27 11:31:44 +0200

    add todoapp component

--- app/app.jsx
+++ app/app.jsx
@@ -1,9 +1,7 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
 var {Route, Router, IndexRoute, hashHistory} = require('react-router');
-var Main = require('Main');
-var Countdown = require('Countdown');
-var Timer = require('Timer');
+var TodoApp = require('TodoApp');
 
 // Load Foundation 5
 $(document).foundation();
@@ -12,6 +10,6 @@ $(document).foundation();
 require('style!css!sass!applicationStyles');
 
 ReactDOM.render(
-    <p>Boilerplate 3 Project</p>,
+    <TodoApp/>,
     document.getElementById('app')
   );
