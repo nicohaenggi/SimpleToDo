16e8974d72fc1d2b25ce84f455e16f9169131f27
Nico Haenggi <nico@haenggi.ch>
2016-05-27 14:07:31 +0200

    add bugfix for add button

--- app/components/AddTodo.jsx
+++ app/components/AddTodo.jsx
@@ -15,9 +15,9 @@ var AddTodo = React.createClass({
   render: function() {
     return (
       <div>
-        <form ref="form" onSubmit={this.onFormSubmit}>
+        <form onSubmit={this.onFormSubmit}>
           <input type="text" ref="todo" placeholder="What do you need to do?"/>
-          <button type="button" className="expanded button">Add Todo</button>
+          <button className="expanded button">Add Todo</button>
         </form>
       </div>
     );
