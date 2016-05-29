cf2ba0a475507e228ab19cd7fd731e8b2cae5f86
Nico Haenggi <nico@haenggi.ch>
2016-05-29 13:25:13 +0200

    style todo application

--- app/components/TodoApp.jsx
+++ app/components/TodoApp.jsx
@@ -55,9 +55,16 @@ var TodoApp = React.createClass({
 
     return (
       <div>
-        <TodoSearch onSearch={this.handleSearch}/>
-        <TodoList todos={filteredTodos} onToggle={this.handleToggle}/>
-        <AddTodo onAddTodo={this.handleAddTodo}/>
+        <h1 className="page-title">Todo App</h1>
+        <div className="row">
+          <div className="small-11 medium-6 large-5 columns small-centered">
+            <div className="container">
+              <TodoSearch onSearch={this.handleSearch}/>
+              <TodoList todos={filteredTodos} onToggle={this.handleToggle}/>
+              <AddTodo onAddTodo={this.handleAddTodo}/>
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
